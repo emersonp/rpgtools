7478a73d79c0b4110860e9d43f597da46d0a0de5
Emerson_Parker <Parker00@HQ.FCTG.NET>
2017-10-10 08:33:55 -0700

    First commit

---
+++ Dice.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Tools
+{
+    public class Dice
+    {
+        public Random rnd = new Random();
+
+        public int Roll(int count, int faces) {
+            int total = 0;
+            for (int i = 0; i < count; i++) {
+                total += rnd.Next(1, faces + 1);
+            }
+            return total;
+        }
+    }
+}
