46c1f488f2ba225b3578b3aef35b2db8c09284bc
Parker Harris Emerson <parker.emerson@gmail.com>
2017-10-10 20:31:08 -0700

    Working on markov chain

--- Markov.cs
+++ Markov.cs
@@ -5,6 +5,29 @@ namespace Tools {
 
     public class MarkovChain 
     {
-        
+        private int _prefixCount {get; set;}
+        public MarkovChain(int prefixCount) {
+            _prefixCount = prefixCount;
+        }
+
+        public void Feed(string[] words) {
+            foreach (string word in words) {
+                
+            }
+        }
+
+        // A chain of letters, including probability
+        private class Chain {
+            private char symbol;
+            private char[] _prefix;
+
+            public Chain(char s) {
+                symbol = s;
+                for (int i = 0; i < _prefixCount; i++) {
+
+                }
+            }
+
+        }
     }
 }
